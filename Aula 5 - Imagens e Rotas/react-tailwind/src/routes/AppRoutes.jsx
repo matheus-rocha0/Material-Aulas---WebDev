049f4e6b6727b7e1636f0ff96676c9fa815ab006
matheusrrocha07 <matheusr0707@gmail.com>
2025-09-04 21:16:55 -0300

    final

---
+++ Aula 5 - Imagens e Rotas/react-tailwind/src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import AdminLayout from "../pages/AdminLayout";
+import Usuarios from "../pages/Usuarios";
+import Dashboard from "../pages/Dashboard";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AdminLayout />,
+        children:[
+            
+            {
+
+                index: true,
+                element: <Dashboard />
+
+            },
+            {
+                path: "/usuarios",
+                element: <Usuarios />   
+            }]
+    }
+])
